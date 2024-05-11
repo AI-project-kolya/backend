cfe53d96eb7e94bbeef4d31e643937b601bc3aa9
SalmaRedaBedair <salmaredab173@gmail.com>
2024-05-11 13:29:44 +0300

    update scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer update ramsey/uuid
 
-echo "generating application key..."
+echo "Running composer"
+composer install  # Use 'install' instead of 'update' to ensure dependencies are installed as expected
+
+echo "Generating application key..."
 php artisan key:generate --show
 
 echo "Caching config..."
@@ -10,4 +11,3 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
-
