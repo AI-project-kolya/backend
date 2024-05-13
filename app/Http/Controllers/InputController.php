d084a44670d2513cd84a82bd33ed6e01aaf6b6d5
SalmaRedaBedair <salmaredab173@gmail.com>
2024-05-13 22:28:20 +0300

    change link

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -12,7 +12,7 @@ class InputController extends Controller
     {
         $userInput =  array_values($request->validated());
 
-        $response = Http::post('http://127.0.0.1:5000/predict_price_range', [
+        $response = Http::post('https://python-ml-test.onrender.com/predict_price_range', [
             'user_input' => $userInput,
         ]);
 
